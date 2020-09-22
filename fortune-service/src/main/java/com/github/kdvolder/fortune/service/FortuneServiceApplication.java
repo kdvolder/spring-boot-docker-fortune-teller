61543262f526f238bc5f6c20315d909b12199552
Kris De Volder <kdevolder@pivotal.io>
2020-09-22 10:40:40 -0700

    Use h2 database for fortunes

--- fortune-service/src/main/java/com/github/kdvolder/fortune/service/FortuneServiceApplication.java
+++ fortune-service/src/main/java/com/github/kdvolder/fortune/service/FortuneServiceApplication.java
@@ -1,21 +1,28 @@
 package com.github.kdvolder.fortune.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.github.kdvolder.fortune.service.data.FortuneMessage;
+import com.github.kdvolder.fortune.service.data.FortuneRepository;
 
 @SpringBootApplication
 @RestController
@@ -24,10 +31,7 @@ import org.springframework.web.client.RestTemplate;
 public class FortuneServiceApplication {
 
 	@Autowired
-	FortuneConfig conf;
-
-	@Autowired
-	private DiscoveryClient eureka;
+	FortuneRepository fortunes;
 
 	Random rnd = new Random();
 	
@@ -37,25 +41,34 @@ public class FortuneServiceApplication {
     }
 
 	@GetMapping(value = "/")
-	public String fortune() {
-		String[] fortunes = conf.getMessages();
-		return fortunes[rnd.nextInt(fortunes.length)];
+	public String random() {
+		//TODO: more efficient way to select a random element?
+		List<FortuneMessage> all = new ArrayList<>();
+		for (FortuneMessage m : fortunes.findAll()) {
+			all.add(m);
+		}
+		return all.get(rnd.nextInt(all.size())).getMessage();
 	}
-
-	@GetMapping("/service-instances/{appName}")
-	public List<ServiceInstance> serviceInstances(@PathVariable String appName) {
-		return eureka.getInstances(appName);
-	}
-	
-	@GetMapping("/test")
-	public String selfTest() {
-		String message = restTemplate().getForObject("http://fortune-service/", String.class);
-		return "Got message: "+message;
-	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(FortuneServiceApplication.class, args);
+	}
+
+	@PostMapping(value = "/fortune")
+	public FortuneMessage create(@RequestBody String message) {
+		FortuneMessage msg = new FortuneMessage();
+		msg.setMessage(message);
+		return fortunes.save(msg);
+	}
+
+	
+	@GetMapping(value = "/fortune/{id}")
+	public FortuneMessage getById(@PathVariable long id) {
+		Optional<FortuneMessage> found = fortunes.findById(id);
+		if (found.isPresent()) {
+			return found.get();
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No fortune with that id");
 	}
 
 }

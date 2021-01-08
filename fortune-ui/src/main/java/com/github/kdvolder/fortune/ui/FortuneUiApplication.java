ca22327374dd0f0e8ba7b5d5ff5e2634b7af2886
Kris De Volder <kdevolder@pivotal.io>
2021-01-08 14:42:40 -0800

    Tweaks

--- fortune-ui/src/main/java/com/github/kdvolder/fortune/ui/FortuneUiApplication.java
+++ fortune-ui/src/main/java/com/github/kdvolder/fortune/ui/FortuneUiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @Controller
+@EnableZuulProxy
 public class FortuneUiApplication {
 	
 	@Bean @LoadBalanced
